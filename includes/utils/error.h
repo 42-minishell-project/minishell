5d4c058a3c3b270b4c1f524ea7a18a27cbf74426
Thxios <jimlee@c6r8s1.42seoul.kr>
2023-09-10 15:00:39 +0900

    fix command find error message

--- includes/utils/error.h
+++ includes/utils/error.h
@@ -6,7 +6,7 @@
 /*   By: jimlee <jimlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 15:18:30 by jimlee            #+#    #+#             */
-/*   Updated: 2023/08/23 15:19:40 by jimlee           ###   ########.fr       */
+/*   Updated: 2023/09/10 14:55:54 by jimlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void	fatal_error(const char *message);
 void	command_find_error(const char *message);
 void	command_permission_error(const char *message);
 void	command_not_found_error(const char *command);
+void	command_directory_error(const char *command);
 
 #endif
--- sources/utils/error.c
+++ sources/utils/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeohong <yeohong@student.42.kr>            +#+  +:+       +#+        */
+/*   By: jimlee <jimlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 23:58:44 by jimlee            #+#    #+#             */
-/*   Updated: 2023/09/05 14:31:14 by yeohong          ###   ########.fr       */
+/*   Updated: 2023/09/10 14:55:43 by jimlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	fatal_error(const char *message)
 {
 	// ft_putstr_fd(info()->exe, STDERR_FILENO);
 	// ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	perror(message);
 	exit(1);
 }
@@ -26,6 +27,7 @@ void	command_find_error(const char *message)
 {
 	// ft_putstr_fd(info()->exe, STDERR_FILENO);
 	// ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	perror(message);
 	exit(127);
 }
@@ -34,6 +36,7 @@ void	command_permission_error(const char *message)
 {
 	// ft_putstr_fd(info()->exe, STDERR_FILENO);
 	// ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	perror(message);
 	exit(126);
 }
@@ -42,7 +45,16 @@ void	command_not_found_error(const char *command)
 {
 	// ft_putstr_fd(info()->exe, STDERR_FILENO);
 	// ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd((char *)command, STDERR_FILENO);
 	ft_putstr_fd(": command not found\n", STDERR_FILENO);
 	exit(127);
+}
+
+void	command_directory_error(const char *command)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd((char *)command, STDERR_FILENO);
+	ft_putstr_fd(": is a directory\n", STDERR_FILENO);
+	exit(126);
 }
