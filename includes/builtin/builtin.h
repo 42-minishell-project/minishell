065f1a08cc38a5b3766e394b2201d087e4b3cb95
Thxios <ljm0302111@naver.com>
2023-09-13 20:36:41 +0900

    main refactored

--- includes/builtin/builtin.h
+++ includes/builtin/builtin.h
@@ -6,7 +6,7 @@
 /*   By: jimlee <jimlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 21:14:55 by yeohong           #+#    #+#             */
-/*   Updated: 2023/09/12 09:31:43 by jimlee           ###   ########.fr       */
+/*   Updated: 2023/09/13 20:36:24 by jimlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 typedef int	(*t_builtin_func)(int, char **);
 
-t_builtin_func	find_builtin_func(const char *arr);
+t_builtin_func	find_builtin_func(const char *name);
 
 #endif
